09fb983726b8ac03f7166ea67706dd4f076246b5
zohre-dev <mehrabi.z13@gmail.com>
2024-09-07 09:35:01 +0330

    build:added delete modal

--- src/modules/Customers/components/table/column.tsx
+++ src/modules/Customers/components/table/column.tsx
@@ -3,12 +3,23 @@ import {
   EditOutlined,
   ExportOutlined,
 } from "@ant-design/icons";
-import { Badge, Button, Flex, TableColumnsType } from "antd";
+import { Badge, Button, Flex, TableColumnsType, Modal } from "antd";
 import ITableItems from "./items";
+import { useState } from "react";
 
 // ant-table-container  , ant-table-cell ,thead/tr/th/ant-table-cell
 
 export const TableColumns = () => {
+  const [open, setOpen] = useState(false);
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleOk = () => {
+    setOpen(false);
+  };
+  const handleCancel = () => {
+    setOpen(false);
+  };
   const columns: TableColumnsType<ITableItems> = [
     {
       title: "#",
@@ -60,7 +71,25 @@ export const TableColumns = () => {
           <Flex align="center" justify="center" gap="small">
             <Button className="tableActionButton" icon={<ExportOutlined />} />
             <Button className="tableActionButton" icon={<EditOutlined />} />
-            <Button className="tableActionButton" icon={<DeleteOutlined />} />
+            <Button
+              className="tableActionButton"
+              icon={<DeleteOutlined />}
+              onClick={() => {
+                Modal.confirm({
+                  title: "Are you sure?",
+
+                  footer: (_, { CancelBtn }) => (
+                    <>
+                      <Button>
+                        <DeleteOutlined />
+                        Delete
+                      </Button>
+                      <CancelBtn />
+                    </>
+                  ),
+                });
+              }}
+            />
           </Flex>
         );
       },
